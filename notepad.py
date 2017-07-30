be718cc1be10eb5e4414a6c266459fb03fe01146
FleetAdmiral <pranavnair97@gmail.com>
2017-07-30 15:31:55 +0530

    Completed

--- notepad.py
+++ notepad.py
@@ -1,3 +1,6 @@
 from pywinauto.application import Application
 # Run a target application
 app = Application().start("notepad.exe")
+app.top_window().menu_select("Help->About Notepad") #to choose menu options
+app.top_window().Ok.click()
+app.top_window().Edit.type_keys("Hello world!", with_spaces=True)
